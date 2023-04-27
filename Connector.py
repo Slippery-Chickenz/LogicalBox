963eebcf80be86c88cd3962ece58690a1851fe79
Kate <carbok@rpi.edu>
2023-04-27 12:09:05 -0400

    Created connector class

--- Connector.py
+++ Connector.py
@@ -1,3 +1,5 @@
+import random
+
 class Connector():
     '''
     class to encapsulate a logical statement defined as
@@ -6,4 +8,16 @@ class Connector():
     &, |, -->
     '''
     def __init__(self, premises):
+
+        #base case
+
+        #recursive call
+        #split premises into two sublists
+        num = random.randint(len(premises)-1)
+        left = premises[0:num]
+        right = premises[num:]
+        self.conn = '&'
+        self.left = Connector(left)
+        self.right = Connector(right)
+
         pass
